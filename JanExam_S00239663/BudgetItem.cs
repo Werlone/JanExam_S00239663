911f38c058c32dd12083873526e9bdf17ad878f6
S00239663 <S00239663@LAB-D2037-10>
2024-01-06 10:42:52 +0000

    Class, List

--- JanExam_S00239663/BudgetItem.cs
+++ JanExam_S00239663/BudgetItem.cs
@@ -6,11 +6,72 @@ using System.Threading.Tasks;
 
 namespace JanExam_S00239663
 {
-    internal class BudgetItem
+    internal class BudgetItem:IComparable<BudgetItem>
     {
+        //attributes
         private string name;
         private decimal amount;
-        private enum budgetitemtype { Income, Expense};
+        private string type;
+        private DateTime date;
+        private bool recurring;
+        
+
+        //constructors
+        public BudgetItem()
+        {
+
+        }
+
+        public BudgetItem(string name, decimal amount, DateTime date, bool recurring, string type)
+        {
+            this.name = name;
+            this.amount = amount;
+            this.date = date;
+            this.recurring = recurring;
+            this.type = type;
+        }
+
+        //IComparable 
+        public int CompareTo(BudgetItem other)
+        {
+            if (this.date.Day > other.date.Day) return 1;
+            else if (this.date.Day < other.date.Day) return -1;
+            else return 0;
+        }
+
+        //get
+        public DateTime Date
+        {
+            get { return date; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public decimal Amount
+        {
+            get { return amount; }
+        }
+
+        public string Type
+        {
+            get { return type; }
+        }
+
+        //recurring
+        public string IsRecurring(bool recurring)
+        {
+            if (recurring) return "Recurring";
+            else return "One-Off";
+        }
+
+        //override to string
+        public override string ToString()
+        {
+            return $"{Date.Day}: {Name} {Amount:c} - {IsRecurring(recurring)}";
+        }
 
     }
 }
--- JanExam_S00239663/MainWindow.xaml.cs
+++ JanExam_S00239663/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace JanExam_S00239663
     /// </summary>
     public partial class MainWindow : Window
     {
+        //global variables
+        static List<BudgetItem> items = new List<BudgetItem>();
+
+        //scuffed dates
+        static DateTime date5 = new DateTime(2002, 5, 5);
+        static DateTime date15 = new DateTime(2002, 5, 15);
+        static DateTime date20 = new DateTime(2002, 5, 20);
+        static DateTime date25 = new DateTime(2002, 5, 25);
+        static DateTime date1 = new DateTime(2002, 5, 1);
+
+
         public MainWindow()
         {
             InitializeComponent();
+            GetData();
         }
+
+        //initial data
+        public void GetData()
+        {
+            //create entries
+            items.Add(new BudgetItem("Grant", 300, date5, true, "income"));
+            items.Add(new BudgetItem("Bonus", 300, date15, false, "income"));
+            items.Add(new BudgetItem("Wages", 100, date25, true, "income"));
+            items.Add(new BudgetItem("Rent", 400, date1, true, "expense"));
+            items.Add(new BudgetItem("Flight", 100, date5, false, "expense"));
+            items.Add(new BudgetItem("Netflix", 10, date15, true, "expense"));
+            items.Add(new BudgetItem("Spotify", 8, date20, true, "expense"));
+
+            //sort entries
+            items.Sort();
+
+
+            //display entries
+            foreach (var item in items)
+            {
+                if (item.Type == "income")
+                    lbIncome.Items.Add(item);
+                else lbExpenses.Items.Add(item);
+            }
+
+
+
+
+        }
+
+        //button on click
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        
     }
 }
