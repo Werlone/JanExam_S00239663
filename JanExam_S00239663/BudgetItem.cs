4a0a9984a9e4ec0466dfee68941ee17dab7f93c3
S00239663 <S00239663@LAB-D2037-10>
2024-01-06 09:54:07 +0000

    WindowDesign

---
+++ JanExam_S00239663/BudgetItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanExam_S00239663
+{
+    internal class BudgetItem
+    {
+        private string name;
+        private decimal amount;
+        private enum budgetitemtype { Income, Expense};
+
+    }
+}
